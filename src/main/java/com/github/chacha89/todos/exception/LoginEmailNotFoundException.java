76870a6e4f2af66a501f0a5696e191d4eeec392b
SKARAIki <132039713+SKARAIki@users.noreply.github.com>
2025-06-16 11:05:50 +0900

    feat: login기능추가, jwtToken발급추가,jwtVerifyToken추가

---
+++ src/main/java/com/github/chacha89/todos/exception/LoginEmailNotFoundException.java
@@ -0,0 +1,7 @@
+package com.github.chacha89.todos.exception;
+
+public class LoginEmailNotFoundException extends RuntimeException{
+    public LoginEmailNotFoundException(){
+        super("아이디 또는 비밀번호가 일치하지 않습니다.");
+    }
+}
--- src/main/java/com/github/chacha89/todos/user/repository/UserRepository.java
+++ src/main/java/com/github/chacha89/todos/user/repository/UserRepository.java
@@ -3,9 +3,14 @@ package com.github.chacha89.todos.user.repository;
 import com.github.chacha89.todos.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
     // 유저서비스의 회원 가입 기능에서 이메일 중복에 사용됨
         // Spring Data JPA의 쿼리 메서드 기능
         // existsBy[필드명] 형식을 자동 인식해서 SQL 쿼리를 생성
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+
 }
