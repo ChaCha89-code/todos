1e646b23eacf8032c5b5984687c99511c168f001
gyogyo0212 <gyogyo0212@gamil.com>
2025-06-16 12:50:27 +0900

    회원 조회부분 작성

---
+++ src/main/java/com/github/chacha89/todos/user/dto/responseDto/UserInfoResponseDto.java
@@ -0,0 +1,34 @@
+package com.github.chacha89.todos.user.dto.responseDto;
+
+
+import lombok.Getter;
+import com.github.chacha89.todos.user.entity.User;
+
+
+public class UserInfoResponseDto {
+    private long id;
+    private String email;
+    private String password;
+    private String userName;
+    private String userImage;
+    private int status;
+    private String message;
+
+
+    public static UserInfoResponseDto from(User user, String message) {
+        UserInfoResponseDto dto = new UserInfoResponseDto();
+        dto.id = user.getId();
+        dto.email = user.getEmail();
+        dto.password = user.getPassword();
+        dto.userName = user.getUserName();
+        dto.userImage = user.getUserImage();
+        dto.status = 200; // 또는 enum/status code 기반으로 설정 가능
+        dto.message = message;
+        return dto;
+    }
+
+}
+
+
+
+
